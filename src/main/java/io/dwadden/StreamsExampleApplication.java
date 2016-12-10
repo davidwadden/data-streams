94056a4f8f1f1ea5ba1d749c890f2b1be87d0b97
David Wadden <dwadden@gmail.com>
2016-12-09 23:41:07 -0600

    Use Spring Initializr to create Gradle project

---
+++ src/main/java/io/dwadden/StreamsExampleApplication.java
@@ -0,0 +1,12 @@
+package io.dwadden;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StreamsExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StreamsExampleApplication.class, args);
+	}
+}
