27f49b8b0875b4b2e57b8099bc6e766321162989
David Wadden <dwadden@gmail.com>
2016-12-10 18:33:20 -0600

    Creates FakeGateway to call Controller

--- src/main/java/io/dwadden/streams/FakeGateway.java
+++ src/main/java/io/dwadden/streams/FakeGateway.java
@@ -2,10 +2,19 @@ package io.dwadden.streams;
 
 import lombok.AccessLevel;
 import lombok.experimental.FieldDefaults;
+import lombok.experimental.NonFinal;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.MediaType;
+import org.springframework.http.RequestEntity;
+import org.springframework.http.ResponseEntity;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 import org.springframework.web.client.RestOperations;
+
+import java.net.URI;
+import java.net.URISyntaxException;
 
 @SuppressWarnings("unused")
 @FieldDefaults(makeFinal = true, level = AccessLevel.PRIVATE)
@@ -15,12 +24,29 @@ public class FakeGateway {
 
     RestOperations restOperations;
 
+    @Value("${fakeGateway.endpoint}")
+    @NonFinal
+    String endpoint;
+
+    @Autowired
     public FakeGateway(RestOperations restOperations) {
         this.restOperations = restOperations;
     }
 
     @Scheduled(fixedDelayString = "${fakeGateway.fixedDelay}")
-    public void uploadPayload() {
-        logger.info("uploading fake payload");
+    public void uploadPayload() throws URISyntaxException {
+        IngestedPayload ingestedPayload = IngestedPayload.builder()
+            .type("some-type")
+            .payload("some-payload")
+            .build();
+
+        RequestEntity<IngestedPayload> requestEntity = RequestEntity
+            .post(new URI("http://localhost:8080/ingest"))
+            .contentType(MediaType.APPLICATION_JSON)
+            .body(ingestedPayload);
+
+        ResponseEntity<String> responseEntity = restOperations.exchange(requestEntity, String.class);
+
+        logger.info("upload response: {}", responseEntity.getStatusCode().name());
     }
 }
---
+++ src/test/java/io/dwadden/streams/FakeGatewayTest.java
@@ -0,0 +1,42 @@
+package io.dwadden.streams;
+
+import lombok.AccessLevel;
+import lombok.experimental.FieldDefaults;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.http.HttpMethod;
+import org.springframework.test.web.client.MockRestServiceServer;
+import org.springframework.test.web.client.response.MockRestResponseCreators;
+import org.springframework.web.client.RestTemplate;
+
+import java.net.URISyntaxException;
+
+import static org.springframework.test.web.client.match.MockRestRequestMatchers.method;
+import static org.springframework.test.web.client.match.MockRestRequestMatchers.requestTo;
+
+@FieldDefaults(level = AccessLevel.PRIVATE)
+class FakeGatewayTest {
+
+    MockRestServiceServer mockServer;
+    FakeGateway fakeGateway;
+
+    @BeforeEach
+    void setUp() {
+        RestTemplate restTemplate = new RestTemplate();
+        mockServer = MockRestServiceServer.createServer(restTemplate);
+        fakeGateway = new FakeGateway(restTemplate);
+    }
+
+    @Test
+    void uploadPayload() throws URISyntaxException {
+        mockServer.expect(requestTo("http://localhost:8080/ingest"))
+            .andExpect(method(HttpMethod.POST))
+            .andRespond(MockRestResponseCreators.withNoContent());
+
+        fakeGateway.uploadPayload();
+
+        mockServer.verify();
+    }
+
+}
