66ad74aac750c71307855bc08de4aaaa0f65d910
David Wadden <dwadden@gmail.com>
2016-12-10 18:40:39 -0600

    Registers RequestDumperFilter to log requests

--- src/main/java/io/dwadden/streams/StreamsExampleConfiguration.java
+++ src/main/java/io/dwadden/streams/StreamsExampleConfiguration.java
@@ -1,10 +1,14 @@
 package io.dwadden.streams;
 
+import org.apache.catalina.filters.RequestDumperFilter;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.web.client.RestOperations;
 import org.springframework.web.client.RestTemplate;
+
+import javax.servlet.Filter;
 
 @SuppressWarnings("unused")
 @EnableScheduling
@@ -14,5 +18,14 @@ public class StreamsExampleConfiguration {
     @Bean
     public RestOperations restOperations() {
         return new RestTemplate();
+    }
+
+    @Bean
+    public FilterRegistrationBean requestDumperFilter() {
+        FilterRegistrationBean registration = new FilterRegistrationBean();
+        Filter requestDumperFilter = new RequestDumperFilter();
+        registration.setFilter(requestDumperFilter);
+        registration.addUrlPatterns("/*");
+        return registration;
     }
 }
