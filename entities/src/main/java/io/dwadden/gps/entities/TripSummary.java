cdfad702f31e222ea58a255f1a9eb1b36125a1cb
David Wadden <dwadden@gmail.com>
2017-02-03 22:17:42 -0600

    Creates POJOs for trip summary and avro-source module

---
+++ entities/src/main/java/io/dwadden/gps/entities/TripSummary.java
@@ -0,0 +1,35 @@
+package io.dwadden.gps.entities;
+
+import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
+import com.fasterxml.jackson.databind.annotation.JsonPOJOBuilder;
+import lombok.Builder;
+import lombok.Value;
+
+import java.util.List;
+
+@JsonDeserialize(builder = TripSummary.TripSummaryBuilder.class)
+@Builder
+@Value
+public class TripSummary {
+
+    Long id;
+    SymbolEnum symbol;
+    List<GyroDetail> gyroDetails;
+
+    @JsonPOJOBuilder(withPrefix = "")
+    public static class TripSummaryBuilder {}
+
+    @JsonDeserialize(builder = GyroDetail.GyroDetailBuilder.class)
+    @Builder
+    @Value
+    public static class GyroDetail {
+        Double roll;
+        Double pitch;
+        Double yaw;
+    }
+
+    public enum SymbolEnum {
+        A, B, C, D
+    }
+
+}
